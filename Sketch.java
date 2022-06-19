dc16e75ad30bd0ca7f264af980350acedb73c289
101itsGabe <105684932+101itsGabe@users.noreply.github.com>
2022-06-19 13:16:21 -0400

    Update Sketch.java
    WORKING Drawing component :)

--- Sketch.java
+++ Sketch.java
@@ -8,23 +8,29 @@ public class Sketch extends JPanel
 {
   private int mx = 0, my = 0;
   private String str;
+  public Vector<Integer> drawx = new Vector<Integer>();
+  public Vector<Integer> drawy = new Vector<Integer>();
 
-
-  public Sketch()
+  public Sketch(JFrame frame)
   {
+   JPanel temp = this;
    addMouseListener(new MouseAdapter()
    {
      Timer timer;
      TimerTask task;
      boolean ifPressed = false;
+     int cx = 0, cy = 0;
      private class MyTimerTask extends TimerTask
      {
       public void run()
       {
        PointerInfo a = MouseInfo.getPointerInfo();
        Point p = a.getLocation();
-       mx = (int) p.getX();
-       my = (int) p.getY();
+
+       mx = (int)p.getX() - (frame.getX() - temp.getX());
+       my = (int)p.getY() - (frame.getY() + 28);
+       drawx.add(mx);
+       drawy.add(my);
        repaint();
       }
      }
@@ -33,14 +39,15 @@ public class Sketch extends JPanel
      {
       timer = new Timer();
       task = new MyTimerTask();
-      timer.schedule(task,0,80);
+      timer.schedule(task,0,10);
      }
 
     public void mouseReleased(MouseEvent e)
     {
-     timer.stop();
+     task.cancel();
+     timer.cancel();
+     timer.purge();
     }
-
 
    });
   }
@@ -49,12 +56,15 @@ public class Sketch extends JPanel
     super.paintComponent(g);            //call superclass's paintComponent
     Graphics2D g2d = (Graphics2D) g;
 
-    str = new String("X: " + mx + "Y: " + my);
+    str = new String("X: " + mx + " Y: " + my);
     g.drawString(str,300,150);
 
     g.setColor(Color.red);
-    g.drawOval(mx,my,5,5);
-    g.fillOval(mx,my,5,5);
+
+    for(int i = 0; i < drawx.size(); i++)
+    {
+       g.fillOval(drawx.get(i),drawy.get(i),5,5);
+    }
 
   }
 
@@ -67,10 +77,10 @@ public class Sketch extends JPanel
 
 
 
-    Sketch sketchJPanel = new Sketch();             //create SketchJPanel
+    Sketch sketchJPanel = new Sketch(frame);             //create SketchJPanel
     frame.add(sketchJPanel);    //add sketchJPanel to the frame
     sketchJPanel.setBackground(Color.lightGray);
-    frame.setSize(600,300);
+    frame.setSize(800,500);
     frame.setVisible(true);
 
 
